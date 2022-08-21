f394d4d2bce909853700d570a2aab5fc7b41188b
babu <neupanebabu828@gmail.com>
2022-08-21 11:57:01 +0930

    notes added

--- core/src/main/java/com/zemeni/core/CoreApplication.java
+++ core/src/main/java/com/zemeni/core/CoreApplication.java
@@ -1,13 +1,22 @@
 package com.zemeni.core;
 
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CoreApplication {
+@Slf4j
+public class CoreApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
+        log.info("Spring boot application is running");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CoreApplication.class);
     }
 }
