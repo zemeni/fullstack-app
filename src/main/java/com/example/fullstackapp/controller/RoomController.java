81a92a269e7c019f2756400371bf2fee78ea7c30
Baburam Neupane <neupanebabu828@gmail.com>
2024-01-09 09:29:49 +0545

    added CRUD from room controller

--- src/main/java/com/example/fullstackapp/controller/RoomController.java
+++ src/main/java/com/example/fullstackapp/controller/RoomController.java
@@ -3,11 +3,8 @@ package com.example.fullstackapp.controller;
 import com.example.fullstackapp.model.Room;
 import com.example.fullstackapp.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,5 +21,25 @@ class RoomController {
     @GetMapping
     public List<Room> findAll(){
         return roomService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Room findRoomById(@PathVariable int id) {
+        return roomService.findRoomById(id);
+    }
+
+    @PostMapping
+    public Room addRoom(@RequestBody Room room) {
+        return roomService.addRoom(room);
+    }
+
+    @PutMapping("/{id}")
+    public Room updateRoom(@RequestBody Room room, @PathVariable int id){
+       return roomService.updateRoom(room, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteRoom(@PathVariable int id){
+        return roomService.deleteRoom(id);
     }
 }
--- src/main/java/com/example/fullstackapp/service/RoomService.java
+++ src/main/java/com/example/fullstackapp/service/RoomService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
@@ -19,5 +20,27 @@ public class RoomService {
 
     public List<Room> findAll() {
         return roomRepository.findAll();
+    }
+
+    public Room addRoom(Room room){
+        return roomRepository.save(room);
+    }
+
+    public Room findRoomById(int id) {
+        Optional<Room> room = roomRepository.findById(id);
+        if(room.isPresent())
+            return room.get();
+        else throw new IllegalArgumentException("room with id "+ id +" not found");
+    }
+
+    public Room updateRoom(Room room, int id) {
+        Room existingRoom = roomRepository.findById(id).orElse(null);
+        existingRoom.setLocation(room.getLocation());
+        return roomRepository.save(existingRoom);
+    }
+
+    public String deleteRoom(int id) {
+        roomRepository.deleteById(id);
+        return "product removed "+id;
     }
 }
